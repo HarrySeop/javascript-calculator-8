d606ba66ee93d36c571a59f88c3b2ad8cfa32f48
HarrySeop <harryseop@gmail.com>
2025-10-15 15:33:05 +0900

    ✨ Feat: 빈 문자열 입력 시 계산 로직 이전에 0을 출력하도록 처리
    - 사용자 입력 문자열을 trim()하여 공백 제거 후, 빈 문자열일 경우 '결과 : 0'을 반환하도록 구현했습니다.
    - 계산 로직을 거치고 0을 반환하는 것 보단 입력받아서 trim() 후 빈 문자열일 경우 바로 0을 반환하는 것이 더 효율적이라고 생각했습니다.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     const userInputString = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    Console.print(userInputString);
+    const trimmedUserInputString = userInputString.trim();
+
+    if (trimmedUserInputString === '') {
+      Console.print('결과 : 0');
+    }
   }
 }
 
