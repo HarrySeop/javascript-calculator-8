fb2f8048d5b7b50ebab19fa9d0fc81573478f7fc
HarrySeop <harryseop@gmail.com>
2025-10-15 16:51:08 +0900

    ✨ Feat: 기본 구분자 기준으로 문자열 분리하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
 
+/**
+ * 기본 구분자(쉼표, 콜론)로 문자열을 나눕니다.
+ * - 선행/후행/연속 구분자 등으로 인해 값이 비어 있으면 Error를 던집니다.
+ * @param {string} trimmedUserInputString - 사용자 입력 문자열(공백 제거된 상태)
+ * @returns {string[]} 구분자를 기준으로 분리된 문자열 배열
+ */
+const splitByDefaultDelimiters = (trimmedUserInputString) => {
+  const splitStrings = trimmedUserInputString.split(/[,:]/);
+
+  const hasEmptyString = splitStrings.some((splitString) => splitString === '');
+  if (hasEmptyString) {
+    throw new Error('[ERROR] 구분자를 기준으로 값이 비어 있습니다.');
+  }
+
+  return splitStrings;
+};
 class App {
   async run() {
     const userInputString = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
@@ -7,7 +23,11 @@ class App {
 
     if (trimmedUserInputString === '') {
       Console.print('결과 : 0');
+      return;
     }
+
+    const splitStrings = splitByDefaultDelimiters(trimmedUserInputString);
+    Console.print(splitStrings);
   }
 }
 
